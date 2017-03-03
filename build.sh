021771fe952e90fb3ddd9e90fa025114a23a7c33
Valentin Vetter <BeLi4L@users.noreply.github.com>
2017-03-03 16:00:48 +0100

    Fix mixed tabs and spaces

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd /project/target
 # We find all source repositories containing a Godeps.json file.
 find . -type f -wholename "*Godeps/Godeps.json" -print0 | while IFS= read -r -d $'\0' line; do
 	# We install the package versions specified in each repositories' Godeps.json
-    cd `dirname $line`/.. && godep restore
+	cd `dirname $line`/.. && godep restore
 done
